88e8fb268098fb3354e8a6228ebfbe14cfdb1716
Cavin Myers <cavin.myers@gmail.com>
2024-09-20 00:06:54 -0400

    Part 9: Cannon Handler and Bomb Path speed fix on level 2

---
+++ Scripts/CannonHandler.gd
@@ -0,0 +1,37 @@
+### CannonHandler.gd
+
+extends Node2D
+
+# when it's loaded into the scene
+func _ready():
+	# play cannon lighting animation on start
+	$Body.play("matching")
+
+func _on_timer_timeout() -> void:
+	# randomizes speech
+	var random_speech = randi() % 3 # will return 0, 1, or 2
+	match random_speech:
+		0:
+			$SpeechBubble.play("boom")
+		1:
+			$SpeechBubble.play("loser")
+		2:
+			$SpeechBubble.play("swearing")
+
+func _process(delta):
+	# randomizes speech bubble randomize time
+	$Timer.wait_time = randi_range(1, 10)
+	
+	# idle animation
+	if Global.is_bomb_moving == true:
+		$Body.play("idle")
+		# show speech bubble
+		$SpeechBubble.visible = true
+	
+	# matching animation
+	if Global.is_bomb_moving == false:
+		$Body.play("matching")
+		# hide speech bubble
+		$SpeechBubble.visible = false
+		
+	
